e7e69fdfcd5fd7e2bdcfb577e34d7986dae0bd6b
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2021-04-26 11:33:22 -0700

    Complete rewrite

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,54 @@
-'use strict';
-// generated on 2014-07-15 using generator-gulp-webapp 0.1.0
+const gulp = require("gulp");
+const { series, parallel } = require("gulp");
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
+var critical = require("critical");
 
-var gulp = require('gulp');
-var critical = require('critical');
+function js() {
+  return gulp.src("./src/**/*.js").pipe(terser()).pipe(gulp.dest("./dist/"));
+}
 
-// load plugins
-var $ = require('gulp-load-plugins')();
+function images() {
+  return gulp
+    .src("./src/**/*.{png,jpg,gif,svg}")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest("./dist/"));
+}
 
-gulp.task('styles', function () {
-    return gulp.src('app/styles/main.css')
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('.tmp/styles'))
-        .pipe($.size());
-});
+function styles() {
+  return gulp.src("./src/**/*.css").pipe(gulp.dest("./dist"));
+}
 
-gulp.task('scripts', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.size());
-});
+function html() {
+  return gulp.src("./src/*.html").pipe(gulp.dest("./dist/"));
+}
 
-gulp.task('html', ['styles', 'scripts'], function () {
-    var jsFilter = $.filter('**/*.js', {restore: true});
-    var cssFilter = $.filter('**/*.css' , {restore: true});
-    var assets = $.useref({searchPath: ['.tmp','app']});
+function criticalCSS(cb) {
+  critical.generate({
+    inline: true,
+    base: "./dist/",
+    src: "../src/index.html",
+    // Output results to file
+    target: {
+      css: "critical.css",
+      html: "index.html",
+      uncritical: "uncritical.css",
+    },
+    minify: true,
+    width: 320,
+    height: 480,
+  });
+  cb();
+}
 
-    return gulp.src('app/*.html')
-        .pipe(assets)
-        .pipe(jsFilter)
-        .pipe($.uglify())
-        .pipe(jsFilter.restore)
-        .pipe(cssFilter)
-        .pipe($.csso())
-        .pipe(cssFilter.restore)
-        .pipe($.useref())
-        .pipe(gulp.dest('dist'))
-        .pipe($.size());
-});
-
-gulp.task('images', function () {
-    return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
-        .pipe($.size());
-});
-
-gulp.task('fonts', function () {
-    return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
-        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-        .pipe($.flatten())
-        .pipe(gulp.dest('dist/fonts'))
-        .pipe($.size());
-});
-
-gulp.task('extras', function () {
-    return gulp.src(['app/*.*', '!app/*.html'], { dot: true })
-        .pipe(gulp.dest('dist'));
-});
-
-
-
-gulp.task('clean', function () {
-    return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
-});
-
-gulp.task('build', ['html', 'images', 'fonts', 'extras']);
-
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
-
-gulp.task('connect', function () {
-    var connect = require('connect'),
-        serveStatic = require('serve-static'),
-        serveIndex = require('serve-index');
-    var app = connect()
-        .use(require('connect-livereload')({ port: 35729 }))
-        .use(serveStatic('app'))
-        .use(serveStatic('.tmp'))
-        .use(serveIndex('app'));
-
-    require('http').createServer(app)
-        .listen(9000)
-        .on('listening', function () {
-            console.log('Started connect web server on http://localhost:9000');
-        });
-});
-
-gulp.task('serve', ['connect'], function () {
-    require('opn')('http://localhost:9000');
-});
-
-
-// Generate & Inline Critical-path CSS
-gulp.task('critical', ['build'], function (cb) {
-    critical.generate({
-        inline: true,
-        base: 'dist/',
-        src: 'index.html',
-        dest: 'dist/index-critical.html',
-        width: 320,
-        height: 480,
-        minify: true
-    });
-});
-
-// inject bower components
-gulp.task('wiredep', function () {
-    var wiredep = require('wiredep').stream;
-
-    gulp.src('app/*.html')
-        .pipe(wiredep({
-            directory: 'app/bower_components'
-        }))
-        .pipe(gulp.dest('app'));
-});
-
-gulp.task('watch', ['connect', 'serve'], function () {
-    var server = $.livereload();
-
-    // watch for changes
-
-    gulp.watch([
-        'app/*.html',
-        '.tmp/styles/**/*.css',
-        'app/scripts/**/*.js',
-        'app/images/**/*'
-    ]).on('change', function (file) {
-        server.changed(file.path);
-    });
-
-    gulp.watch('app/styles/**/*.css', ['styles']);
-    gulp.watch('app/scripts/**/*.js', ['scripts']);
-    gulp.watch('app/images/**/*', ['images']);
-    gulp.watch('bower.json', ['wiredep']);
-});
+exports.default = parallel(js, images, styles, html);
+exports.critical = series(parallel(js, images, styles, html), criticalCSS);
